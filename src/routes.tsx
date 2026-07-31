d8f6b2ae6c667fd635e30af8c0a7453b0c361cab
Lucas Torres <eu.lucastr@gmail.com>
2026-07-30 22:20:25 -0300

    fix import error

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { AppLayout } from './layouts/app-layout';
-import { DashboardPage } from './pages/DashboardPage';
+// import { DashboardPage } from './pages/DashboardPage';
 import { TimerPage } from './pages/TimerPage';
 
 export const router = createBrowserRouter([
