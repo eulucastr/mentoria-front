35a3094c5c9943aadec5ea46bfdf10311f491f4d
Lucas Torres <eu.lucastr@gmail.com>
2026-07-30 22:15:28 -0300

    update file name

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom';
-import { AppLayout } from './layouts/AppLayout';
+import { AppLayout } from './layouts/app-layout';
 import { DashboardPage } from './pages/DashboardPage';
 import { TimerPage } from './pages/TimerPage';
 
